894f54112dd72171cc3360e8f51ddb6364c40d3a
vodofrede <frederik@palmoe.dk>
2024-12-05 16:48:10 +0100

    2024 day 5 part 2

--- src/bin/2024-05.rs
+++ src/bin/2024-05.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 fn main() {
     let input = fs::read_to_string("input/2024-05.txt").unwrap();
@@ -10,36 +13,52 @@ fn part1(input: &str) {
     // parse
     let (rules, updates) = input.split_once("\n\n").unwrap();
     let rules = rules.lines().filter_map(|l| l.split_once('|')).fold(
-        HashMap::new(),
+        HashMap::<u64, HashSet<u64>>::new(),
         |mut rules, (before, after)| {
             rules
-                .entry(before)
-                .and_modify(|v: &mut Vec<_>| v.push(after))
-                .or_default();
+                .entry(after.parse().unwrap())
+                .or_default()
+                .insert(before.parse().unwrap());
             rules
         },
     );
+    let updates = updates.lines().map(|l| {
+        l.split(',')
+            .map(|v| v.parse::<u64>().unwrap())
+            .collect::<Vec<_>>()
+    });
+    let valid = updates.filter(|pages| pages.is_sorted_by(|a, b| rules[b].contains(a)));
+    let sum = valid.map(|pages| pages[(pages.len() - 1) / 2]).sum::<u64>();
 
-    let valid = updates
-        .lines()
-        .map(|l| l.split(',').collect::<Vec<_>>())
-        .filter(|pages| {
-            for i in 0..pages.len() {
-                let page = pages[i];
-                let left = &pages[..i];
-                if let Some(rules) = rules.get(page) {
-                    if rules.iter().any(|after| left.contains(after)) {
-                        return false;
-                    }
-                }
-            }
-            true
-        });
+    println!("part 1: {sum}");
+}
 
-    let sum = valid
-        .map(|pages| pages[(pages.len() - 1) / 2].parse::<u64>().unwrap())
+fn part2(input: &str) {
+    // parse
+    let (rules, updates) = input.split_once("\n\n").unwrap();
+    let rules = rules.lines().filter_map(|l| l.split_once('|')).fold(
+        HashMap::<u64, HashSet<u64>>::new(),
+        |mut rules, (before, after)| {
+            rules
+                .entry(after.parse().unwrap())
+                .or_default()
+                .insert(before.parse().unwrap());
+            rules
+        },
+    );
+    let updates = updates.lines().map(|l| {
+        l.split(',')
+            .map(|v| v.parse::<u64>().unwrap())
+            .collect::<Vec<_>>()
+    });
+    let invalid = updates.filter(|pages| !pages.is_sorted_by(|a, b| rules[b].contains(a)));
+    let sorted = invalid.map(|mut pages| {
+        pages.sort_by(|a, b| rules[b].contains(a).cmp(&true));
+        pages
+    });
+    let sum = sorted
+        .map(|pages| pages[(pages.len() - 1) / 2])
         .sum::<u64>();
 
     println!("part 1: {sum}");
 }
-fn part2(input: &str) {}
