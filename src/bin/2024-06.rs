976e8c173ddd2352411757094adf186d6a1b0148
vodofrede <frederik@palmoe.dk>
2024-12-06 16:18:48 +0100

    2024 day 6
    use hashmap instead of custom struct

--- src/bin/2024-06.rs
+++ src/bin/2024-06.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashSet, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 fn main() {
     let input = fs::read_to_string("input/2024-06.txt").unwrap();
@@ -6,43 +9,24 @@ fn main() {
     part2(&input);
 }
 
-#[derive(Debug, Clone)]
-struct Map {
-    grid: Vec<Vec<char>>,
-    dims: (usize, usize),
-}
-impl Map {
-    fn contains(&self, (x, y): (isize, isize)) -> bool {
-        0 <= x && x < self.dims.0 as isize && 0 <= y && y < self.dims.1 as isize
-    }
-    fn get(&self, (x, y): (isize, isize)) -> Option<char> {
-        self.contains((x, y))
-            .then(|| self.grid.get(y as usize)?.get(x as usize).copied())?
-    }
-    fn get_mut(&mut self, (x, y): (isize, isize)) -> Option<&mut char> {
-        self.contains((x, y))
-            .then(|| self.grid.get_mut(y as usize)?.get_mut(x as usize))?
-    }
-}
 fn part1(input: &str) {
-    let grid = input
+    let map = input
         .lines()
-        .map(|l| l.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-    let dims = (grid[0].len(), grid.len());
-    let mut guard = (0..grid.len())
-        .find_map(|y| {
-            (0..grid[y].len()).find_map(|x| (grid[y][x] == '^').then_some((x as isize, y as isize)))
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.chars()
+                .enumerate()
+                .map(move |(x, c)| ((x as isize, y as isize), c))
         })
-        .unwrap();
+        .collect::<HashMap<_, _>>();
+    let mut guard = *map.iter().find(|(_, c)| **c == '^').unwrap().0;
     let mut direction = (0, -1);
-    let map = Map { grid, dims };
     let visited = (0..)
         .map_while(|_| loop {
             let (x, y) = guard;
             let (dx, dy) = direction;
             let (nx, ny) = (x + dx, y + dy);
-            let next = map.get((nx, ny))?;
+            let next = map.get(&(nx, ny))?;
             match next {
                 '.' | '^' => {
                     guard = (nx, ny);
@@ -60,19 +44,22 @@ fn part1(input: &str) {
     println!("part 1: {visited}");
 }
 fn part2(input: &str) {
-    let grid = input
+    let map = input
         .lines()
-        .map(|l| l.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-    let dims = (grid[0].len(), grid.len());
-    let guard = (0..grid.len())
-        .find_map(|y| {
-            (0..grid[y].len()).find_map(|x| (grid[y][x] == '^').then_some((x as isize, y as isize)))
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.chars()
+                .enumerate()
+                .map(move |(x, c)| ((x as isize, y as isize), c))
         })
-        .unwrap();
-    let map = Map { grid, dims };
+        .collect::<HashMap<_, _>>();
+    let guard = *map.iter().find(|(_, c)| **c == '^').unwrap().0;
 
-    fn walk(map: &Map, start: (isize, isize), simulation: bool) -> Option<HashSet<(isize, isize)>> {
+    fn walk(
+        map: &HashMap<(isize, isize), char>,
+        start: (isize, isize),
+        simulation: bool,
+    ) -> Option<HashSet<(isize, isize)>> {
         let mut obstacles = HashSet::new();
         let mut visited = HashSet::new();
 
@@ -84,11 +71,11 @@ fn part2(input: &str) {
             }
             visited.insert(((x, y), (dx, dy)));
             let (nx, ny) = (x + dx, y + dy);
-            match map.get((nx, ny)) {
+            match map.get(&(nx, ny)) {
                 Some('.') | Some('^') => {
                     if !simulation && !obstacles.contains(&(nx, ny)) && (x, y) != start {
                         let mut map = map.clone();
-                        *map.get_mut((nx, ny))? = '#';
+                        *map.get_mut(&(nx, ny))? = '#';
                         if walk(&map, start, true).is_none() {
                             obstacles.insert((nx, ny));
                         }
